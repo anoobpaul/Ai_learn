cf1bd34b2fa45f1e067d99e17c88ba0150604576
Anoob Paul <155704736+anoobpaul@users.noreply.github.com>
2024-02-19 16:39:02 +0530

    Update geminiconnect.py
    updated comments

--- geminiconnect.py
+++ geminiconnect.py
@@ -1,9 +1,14 @@
 import google.generativeai as genai
 
-
-google_api_key = 'AIzaS12132342343453534543KN57cRgw5Bno'
+# to generate the api key, login  to  https://aistudio.google.com/app/apikey
+google_api_key = 'AIzaSyAINogrrbtIUVBJBH8787cRgw5Bno'
 genai.configure(api_key=google_api_key)
 
+# below code will print the available models names. for testing we use the gemini-pro model 
+# for m in genai.list_models():
+#   if 'generateContent' in m.supported_generation_methods:
+#     print(m.name)
+
 model =genai.GenerativeModel('gemini-pro')
-response = model.generate_content("Write a java program to add 2 integers?") 
+response = model.generate_content("give me 5 points on the future of AI ?") 
 print(response.text)  
