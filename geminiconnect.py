d3be8fb119ca32b8d68ef8a4722f901759db3e2f
Anoob Paul <155704736+anoobpaul@users.noreply.github.com>
2024-02-19 22:39:14 +0530

    Update geminiconnect.py

--- geminiconnect.py
+++ geminiconnect.py
@@ -1,14 +1,28 @@
 import google.generativeai as genai
+from IPython.display import Markdown
+import textwrap
 
 # to generate the api key, login  to  https://aistudio.google.com/app/apikey
-google_api_key = 'AIzaSyAINogrrbtIUVBJBH8787cRgw5Bno'
+google_api_key = 'AIzaSyrerGHJBI8678687jbKJB7cRgw5Bno'
 genai.configure(api_key=google_api_key)
 
-# below code will print the available models names. for testing we use the gemini-pro model 
+
 # for m in genai.list_models():
 #   if 'generateContent' in m.supported_generation_methods:
 #     print(m.name)
 
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '', predicate=lambda _: True))
+
+
+
 model =genai.GenerativeModel('gemini-pro')
-response = model.generate_content("give me 5 points on the future of AI ?") 
-print(response.text)  
+response = model.generate_content("where is banglore ?") 
+
+print(response.text)
+
+markdown_text  = to_markdown(response.text)
+print(markdown_text.data)
+
